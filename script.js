0321b2294a2b54001499282584847d2e2be8aa57
GiuseppeLiGioi <pepe2117k@gmail.com>
2025-05-13 12:20:03 +0200

    added. snack 5 (bonus)

--- script.js
+++ script.js
@@ -127,14 +127,40 @@ console.log(ageAvg)
 
 /*
 Snack 5 (Bonus) - Raccogli i libri
-Nota: a differenza di quanto visto finora negli esempi, per accedere all'API utilizzare utilizzare l'url base:
-https://boolean-spec-frontend.vercel.app/freetestapi
+Nota: Per accedere all'API utilizzare utilizzare l'url base:
+http://localhost:5000/
 al posto di:
 https://freetestapi.com/api/v1
-
 Ad esempio:
-https://boolean-spec-frontend.vercel.app/freetestapi/users
-per chiamare l'endpoint /users
-Usando la l'API https://boolean-spec-frontend.vercel.app/freetestapi/books/{id} usa la combinazione di .map() e Promise.all(), per creare una funzione (getBooks) che a partire da un array di id (ids), ritorna una promise che risolve un array di libri (books).
+http://localhost:5000/destinations
+per chiamare l'endpoint /destinations
+Usando la l'API https://boolean-spec-frontend.vercel.app/freetestapi/books/{id} usa la combinazione di .map() e Promise.all(), per creare una funzione (getBooks) 
+che a partire da un array di id (ids), ritorna una promise che risolve un array di libri (books).
 Testala con l’array [2, 13, 7, 21, 19] .
 */
+
+//creo la funzione fetchjson per facilitarmi il lavoro e rendere il codice più snello, senza await perchè se no ottengo già la resolve
+async function fetchJson(url){
+	const response = await fetch(url);
+	const obj = response.json()
+	return obj;
+}
+
+
+const endpoints = [2, 13, 7, 21, 19];
+async function getBooks(arr){
+const promises = arr.map((e) => {
+	return fetchJson(`http://localhost:5000/books/${e}`)
+})
+ return Promise.all(promises)
+.then(results => {
+	console.log(results)
+})
+.catch((error) => {
+	console.error(error);
+})
+}
+
+getBooks(endpoints);
+
+
