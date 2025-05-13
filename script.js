429d293dece1f073e0248bb4062e290a75c2fc0e
GiuseppeLiGioi <pepe2117k@gmail.com>
2025-05-13 11:30:13 +0200

    added. snack 4

--- script.js
+++ script.js
@@ -106,3 +106,21 @@ authors.sort((a,b) => {
 })
 console.log(authors)
 */
+
+/*
+Snack 4 - Calcola l’età media
+Creare un array (ages) che contiene le età degli autori dei libri.
+Calcola la somma delle età (agesSum) usando reduce.
+Stampa in console l’età media degli autori dei libri.
+*/
+
+const ages = books.map((b) => b.author.age);
+
+const agesSum = ages.reduce((acc, curr) => {
+return acc + curr;
+}, 0)
+console.log(agesSum);
+
+ageAvg = agesSum / ages.length;
+console.log(ageAvg)
+
